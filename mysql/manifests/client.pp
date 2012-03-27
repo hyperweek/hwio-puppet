4604c6502b7eb0a42c4e2cb93623146373179ac9
zyegfryed <zyegfryed@gmail.com>
2012-03-27 11:02:40 +0200

    Database name are now quoted.

--- mysql/manifests/client.pp
+++ mysql/manifests/client.pp
@@ -10,13 +10,13 @@ class mysql::client {
     $options = "-h ${::mysql_host} -P ${::mysql_port} -u${::mysql_username} -p${::mysql_password}"
 
     exec { "create-db-${name}":
-      unless  => "/usr/bin/mysql $options ${name}",
-      command => "/usr/bin/mysql $options -e \"CREATE DATABASE IF NOT EXISTS ${name} CHARSET utf8 COLLATE utf8_unicode_ci;\"",
+      unless  => "/usr/bin/mysql $options '${name}'",
+      command => "/usr/bin/mysql $options -e \"CREATE DATABASE IF NOT EXISTS `${name}` CHARSET utf8 COLLATE utf8_unicode_ci;\"",
     }
 
     exec { "grant-db-${name}":
-      unless  => "/usr/bin/mysql -h ${::mysql_host} -P ${::mysql_port} -u${user} -p${password} ${name}",
-      command => "/usr/bin/mysql $options -e \"GRANT ALL ON ${name}.* TO '${user}'@'%' IDENTIFIED BY '$password';UPDATE mysql.user SET Password=PASSWORD('$password') where USER='$user';FLUSH PRIVILEGES;\"",
+      unless  => "/usr/bin/mysql -h ${::mysql_host} -P ${::mysql_port} -u${user} -p${password} '${name}'",
+      command => "/usr/bin/mysql $options -e \"GRANT ALL ON `${name}`.* TO '${user}'@'%' IDENTIFIED BY '$password';UPDATE mysql.user SET Password=PASSWORD('$password') where USER='$user';FLUSH PRIVILEGES;\"",
     }
 
     Package['mysql-client'] -> Exec["create-db-${name}"] -> Exec["grant-db-${name}"]
