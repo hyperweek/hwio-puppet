bb5675c00171f12a448297dd067de5bdf8af7ae4
zyegfryed <zyegfryed@gmail.com>
2012-03-28 10:28:43 +0200

    Mysql quoting character should be escaped.

--- mysql/manifests/client.pp
+++ mysql/manifests/client.pp
@@ -11,12 +11,12 @@ class mysql::client {
 
     exec { "create-db-${name}":
       unless  => "/usr/bin/mysql $options '${name}'",
-      command => "/usr/bin/mysql $options -e \"CREATE DATABASE IF NOT EXISTS `${name}` CHARSET utf8 COLLATE utf8_unicode_ci;\"",
+      command => "/usr/bin/mysql $options -e \"CREATE DATABASE IF NOT EXISTS \\`${name}\\` CHARSET utf8 COLLATE utf8_unicode_ci;\"",
     }
 
     exec { "grant-db-${name}":
       unless  => "/usr/bin/mysql -h ${::mysql_host} -P ${::mysql_port} -u${user} -p${password} '${name}'",
-      command => "/usr/bin/mysql $options -e \"GRANT ALL ON `${name}`.* TO '${user}'@'%' IDENTIFIED BY '$password';UPDATE mysql.user SET Password=PASSWORD('$password') where USER='$user';FLUSH PRIVILEGES;\"",
+      command => "/usr/bin/mysql $options -e \"GRANT ALL ON \\`${name}\\`.* TO '${user}'@'%' IDENTIFIED BY '$password';UPDATE mysql.user SET Password=PASSWORD('$password') where USER='$user';FLUSH PRIVILEGES;\"",
     }
 
     Package['mysql-client'] -> Exec["create-db-${name}"] -> Exec["grant-db-${name}"]
